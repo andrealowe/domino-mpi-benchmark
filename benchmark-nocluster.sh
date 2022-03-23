6031bf40407d05db9ebd12ff21a47c1ae863eb03
andrealowe <51382000+andrealowe@users.noreply.github.com>
2022-03-23 14:45:32 -0700

    Update benchmark-nocluster.sh

--- benchmark-nocluster.sh
+++ benchmark-nocluster.sh
@@ -3,6 +3,6 @@
 file_loc=${1:-/mnt/SSD-results/single-gpu/train_log}
 GPUS=${2:-1}
 
-sed -n 's|.*global_step/sec: \(\S\+\).*|\1|p' $file_loc | python -c "import sys; x = sys.stdin.readlines();  x = [float(a) for a in x[int(len(x)*3/4):]]; print(32*$GPUS*sum(x)/len(x), 'img/s')"
+PERF=$(sed -n 's|.*global_step/sec: \(\S\+\).*|\1|p' $file_loc | python -c "import sys; x = sys.stdin.readlines();  x = [float(a) for a in x[int(len(x)*3/4):]]; print(32*$GPUS*sum(x)/len(x), 'img/s')")
 
-echo "$GPUS GPU mixed precision training performance: $PERF" >> $file_loc
+echo "$GPUS GPU mixed precision training performance: $PERF" | tee -a $file_loc
