e43079bb1c31ccb61ece2ad45c272fdb2e7d5658
andrealowe <51382000+andrealowe@users.noreply.github.com>
2022-03-23 14:36:55 -0700

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,8 @@
-a#!/bin/bash
+#!/bin/bash
 
 GPUS=${1:-4}
 file_loc=${2:-/mnt/SSD-results/multi-gpu/train_log}
 
-
 sed -n 's|.*global_step/sec: \(\S\+\).*|\1|p' $file_loc | python -c "import sys; x = sys.stdin.readlines();  x = [float(a) for a in x[int(len(x)*3/4):]]; print(32*$GPUS*sum(x)/len(x), 'img/s')"
 
-echo "$GPUS GPUs mixed precision training performance: $PERF" >> $file_locmixed precision training performance: $PERF" >> $file_loc
+echo "$GPUS GPUs mixed precision training performance: $PERF" >> $file_loc 
