0d568afa0210dfff87c1fbdb7da2ec354be2742f
andrealowe <51382000+andrealowe@users.noreply.github.com>
2022-03-23 14:53:38 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -13,7 +13,8 @@
 # limitations under the License.
 
 CKPT_DIR=${1:-"/mnt/SSD-results/single-gpu"}
-PIPELINE_CONFIG_PATH=${2:-"/mnt/SSD/configs"}"/ssd320_bench.config"
+PIPELINE_CONFIG_PATH=${2:-"/mnt/SSD/configs/ssd320_bench.config"}
+LOG_FILE=${3:-"train_log"}
 GPUS=1
 
 TENSOR_OPS=0
@@ -23,20 +24,14 @@ export TF_ENABLE_CUDNN_RNN_TENSOR_OP_MATH_FP32=${TENSOR_OPS}
 
 #edited for Domino:
 
+mkdir -p $CKPT_DIR
+
 pushd /mnt/SSD/models/research
 
-sudo chmod +x /usr/local/bin/protoc
-protoc object_detection/protos/*.proto --python_out=.
-
-TRAIN_LOG=$(python -u ./object_detection/model_main.py \
+python -u ./object_detection/model_main.py \
        --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
        --model_dir=${CKPT_DIR} \
        --alsologtostder \
        --amp \
-       "${@:3}" 2>&1)
+       "${@:3}" 2>&1 | tee $CKPT_DIR/$LOG_FILE
 
-PERF=$(echo "$TRAIN_LOG" | sed -n 's|.*global_step/sec: \(\S\+\).*|\1|p' | python -c "import sys; x = sys.stdin.readlines(); x = [float(a) for a in x[int(len(x)*3/4):]]; print(32*$GPUS*sum(x)/len(x), 'img/s')")
-
-mkdir -p $CKPT_DIR
-echo "Single GPU mixed precision training performance: $PERF" | tee $CKPT_DIR/train_log
-echo "$TRAIN_LOG" >> $CKPT_DIR/train_log
